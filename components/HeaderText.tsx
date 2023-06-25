869b74065e078a43749780853e3db2e5ae9dded6
najmeh98 <foroughi.najmeh98@gmail.com>
2023-06-25 13:43:15 +0330

    Add header component

--- components/HeaderText.tsx
+++ components/HeaderText.tsx
@@ -1,9 +1,7 @@
-import { ReactNode } from "react";
-
-interface IProp {
-  children: ReactNode;
-  className?: string;
-}
-export const Text = ({ children, className }: IProp): JSX.Element => {
-  return <p className={className}>{children}</p>;
+export const HeaderText = ({ text }: { text: string }) => {
+  return (
+    <p className="bg-clip-text  bg-gradient-to-r from-blue-500 from-10% via-indigo-500 via-30% to-pink-600 to-90% text-transparent flex items-center justify-center w-full  font-semibold text-3xl capitalize ">
+      {text && text.length > 0 ? `welcome ${text}` : `welcome`}
+    </p>
+  );
 };
