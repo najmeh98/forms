2250224a19094a63d2ce6764e4b31aa91df9d80c
najmeh98 <foroughi.najmeh98@gmail.com>
2023-06-25 13:45:06 +0330

    Added Schema for phone number

--- components/Validation/index.ts
+++ components/Validation/index.ts
@@ -1,22 +1,29 @@
 import * as Yup from "yup";
-import { ref, string } from "yup";
+import { object, ref, string } from "yup";
 
 const randomRegex: RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+const phoneRegExp =
+  /[0۰][9۹]([0-39۹\u06F0-\u06F3])[0-9\u06F0-\u06F9]-?[0-9\u06F0-\u06F9]{3}-?[0-9\u06F0-\u06F9]{4}/gu;
 
 const getCharacterValidationError = (str: string): string => {
   return `Your password must have at least 1 ${str} character`;
 };
 
-export const SignUpSchema = Yup.object({
-  userName: Yup.string()
+export const SignUpSchema = object({
+  userName: string()
     .required("Please Enter your name!")
     .min(4, "Must be longer than 2 characters")
     .max(12, "Nice try, nobody has a first name that long"),
 
-  email: Yup.string()
+  email: string()
     .email()
     .required("Please Enter your email !")
     .matches(randomRegex, "Invalid email address"),
+
+  phoneNumber: string()
+    .required("Please Enter your phoneNumber!")
+    .matches(phoneRegExp, "Invalid phone Number"),
 
   password: string()
     .required("Please enter a password")
@@ -25,7 +32,7 @@ export const SignUpSchema = Yup.object({
     .matches(/[a-z]/, getCharacterValidationError("lowercase"))
     .matches(/[A-Z]/, getCharacterValidationError("uppercase")),
 
-  confirmpassword: string()
+  confirmPassword: string()
     .required("Please re-type your password")
     .oneOf([ref("password")], "Passwords does not match"),
 });
--- types/signup.interface.ts
+++ types/signup.interface.ts
@@ -1,6 +1,7 @@
 export interface ISignupParams {
   userName: string;
   email: string;
+  phoneNumber: number;
   password: string;
   confirmPassword: string;
 }
